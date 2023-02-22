b45d800f8736723165cef24048f072de49008e79
Dina <dina.yamanid@gmail.com>
2023-02-22 18:08:28 +0100

    calculadora

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -6,7 +6,7 @@
 <script setup>
 import NavDos from "../components/NavDos.vue"
 import Footer from "../components/Footer.vue"
-import Calculadroa from "../view/Calculadora.vue"
+import Calculadora from "../components/Calculadora.vue"
 </script>
 <style>
 </style>
